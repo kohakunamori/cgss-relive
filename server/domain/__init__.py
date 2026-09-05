1a6422c81eb4c4ac05977673c5e380989f0c4868
Kohaku Namori <2633047525@qq.com>
2026-09-05 14:40:31 +0900

    server: export unit membership domain service

--- server/domain/__init__.py
+++ server/domain/__init__.py
@@ -42,6 +42,7 @@ from .services import (
     PreservationProfileService,
     StarterCardGrant,
 )
+from .unit_services import PreservationUnitService, UnitMembershipUpdate
 
 __all__ = [
     "BootstrapPolicy",
@@ -65,6 +66,7 @@ __all__ = [
     "PlayerResource",
     "PlayerStateRepository",
     "PreservationProfileService",
+    "PreservationUnitService",
     "RandomSource",
     "ResourceChange",
     "Reward",
@@ -77,4 +79,5 @@ __all__ = [
     "SystemClock",
     "Unit",
     "UnitMember",
+    "UnitMembershipUpdate",
 ]
